5e42d6fc05dd301760f7b6eed3410821c0819762
YKSS\huizeyang <huizeyang@66y.com>
2024-07-15 18:28:58 +0800

    feat: add token class

---
+++ Token.cpp
@@ -0,0 +1,5 @@
+//
+// Created by huizeyang on 24-7-15.
+//
+
+#include "Token.h"
---
+++ Token.h
@@ -0,0 +1,16 @@
+//
+// Created by huizeyang on 24-7-15.
+//
+
+#ifndef TOKEN_H
+#define TOKEN_H
+
+
+
+class Token {
+
+};
+
+
+
+#endif //TOKEN_H
