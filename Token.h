d7d4ab03847fc051f7280bf969c1a7d487682200
YKSS\huizeyang <huizeyang@66y.com>
2024-07-15 19:04:15 +0800

    feat: def token types

--- Token.h
+++ Token.h
@@ -6,11 +6,30 @@
 #define TOKEN_H
 
 
+constexpr enum TOKENS {
+    TOKEN_DEF_BEGIN,
 
-class Token {
+    ID,
+    COMMENT,
+    LITERAL,
 
+    // keywords
+    EXTERN,
+
+    // marks
+    GREATER_THAN, // >
+    QUESTION, // ?
+    BLACK_TICK, // `
+    CHARP, // #
+    L_BRACKET, // {
+    R_BRACKET, // }
+    VERTICAL_BAR, // |
+    ARROW, // =>
+
+    EXTERN_TYPE,
+
+    TOKEN_DEF_END,
 };
 
 
-
-#endif //TOKEN_H
+#endif // TOKEN_H
