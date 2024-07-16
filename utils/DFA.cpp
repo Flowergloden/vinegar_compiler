0dfc129b5df184bc707abc7e87450ca4329e8b42
YKSS\huizeyang <huizeyang@66y.com>
2024-07-16 19:02:16 +0800

    feat: impl basic dfa parser

--- utils/DFA.cpp
+++ utils/DFA.cpp
@@ -3,3 +3,34 @@
 //
 
 #include "DFA.h"
+DFA::DFA(const std::vector<DFARaw> &src)
+{
+    for (auto item : src)
+    {
+        auto name = item.name;
+        auto raw_pattern = item.pattern;
+
+        int total_state{1};
+        int state_now{0};
+        for (auto chr : raw_pattern)
+        {
+            if (chr == ' ') continue;
+
+            bool has_same{false};
+            for (auto unit : state_move_matrix)
+            {
+                if (unit.state == state_now && unit.cond == chr)
+                {
+                    has_same = true;
+                    state_now = unit.next_state;
+                    break;
+                }
+            }
+
+            if (has_same) continue;
+
+            state_move_matrix.push_back({state_now, chr, total_state});
+            ++total_state;
+        }
+    }
+}
--- utils/DFA.h
+++ utils/DFA.h
@@ -5,12 +5,29 @@
 #ifndef DFA_H
 #define DFA_H
 
+#include <vector>
 #include "Token.h"
 
-class DFA {
+struct DFARaw
+{
+    std::string_view name;
+    std::string_view pattern;
+};
 
+class DFA
+{
+public:
+    explicit DFA(const std::vector<DFARaw>& src);
+
+private:
+    struct StateMoveUnit
+    {
+        int state;
+        char cond;
+        int next_state;
+    };
+    std::vector<StateMoveUnit> state_move_matrix;
 };
 
 
-
-#endif //DFA_H
+#endif // DFA_H
