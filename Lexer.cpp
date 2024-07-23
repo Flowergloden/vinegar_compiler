ee8624abb51354e7e4324cce33850ca480f2b47d
YKSS\huizeyang <huizeyang@66y.com>
2024-07-23 16:25:32 +0800

    feat: impl lexer

--- Lexer.cpp
+++ Lexer.cpp
@@ -3,3 +3,34 @@
 //
 
 #include "Lexer.h"
+Lexer::Lexer(DFA &dfa, const std::string_view raw) : dfa(dfa)
+{
+    auto chr = raw.begin();
+    const auto end = raw.end();
+    while (chr != end)
+    {
+        std::string lexeme{};
+        const int final_state{this->dfa.scan_move(lexeme, chr, end)};
+
+        const TOKEN_TYPE type{this->dfa.get_token_type(final_state)};
+        if (type == UNKNOWN_TOKEN)
+            std::cerr << "Unknow token: " << lexeme << std::endl;
+
+        if (token_table.contains(lexeme))
+        {
+            tokens.push_back(token_table[lexeme]);
+        }
+        else
+        {
+            tokens.push_back({type, lexeme});
+        }
+    }
+}
+void Lexer::print_all_tokens()
+{
+    for (auto [token_type, lexeme, value, type] : tokens)
+    {
+        std::cout << "token type: " << token_type << "lexeme: " << lexeme << "value: " << value << "type: " << type
+                  << std::endl;
+    }
+}
--- Lexer.h
+++ Lexer.h
@@ -4,10 +4,22 @@
 
 #ifndef LEXER_H
 #define LEXER_H
+#include "utils/DFA.h"
+#include "utils/Token.h"
 
 
 class Lexer
 {
+public:
+    explicit Lexer(DFA& dfa, std::string_view raw);
+
+    std::vector<Token> tokens{};
+
+    // DEBUG ONLY
+    void print_all_tokens();
+
+private:
+    DFA& dfa;
 };
 
 
