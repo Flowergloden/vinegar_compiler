215a22748fff9e77eaca21656d0811b23f787e3e
YKSS\huizeyang <huizeyang@66y.com>
2024-07-23 17:46:28 +0800

    chore: main function

--- main.cpp
+++ main.cpp
@@ -1,17 +1,26 @@
 #include <iostream>
+#include <memory>
 #include <vector>
 
+#include "Lexer.h"
 #include "utils/DFA.h"
 #include "utils/Token.h"
 
 int main()
 {
     const std::vector<DFARaw> dfa_samples{
-        {GREATER_THAN, ">"},
+        {LITERAL, " (True) | (False) "},
+        {ID, "[a - z]+"},
+        {COMMENT, "//"},
     };
 
-    DFA dfa(dfa_samples);
-    dfa.test_dfa();
+    const std::string raw_code{"True False abc //123"};
+
+    const std::unique_ptr<DFA> dfa{new DFA(dfa_samples)};
+    dfa->test_dfa();
+
+    const std::unique_ptr<Lexer> lexer{new Lexer(*dfa, raw_code)};
+    lexer->print_all_tokens();
 
     return 0;
 }
