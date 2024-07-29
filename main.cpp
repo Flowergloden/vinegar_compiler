42bfc8388d9c15c2ca42fb8d411ff348663d6c41
YKSS\huizeyang <huizeyang@66y.com>
2024-07-29 19:49:37 +0800

    feat: add code file && chore: main function call

--- main.cpp
+++ main.cpp
@@ -9,29 +9,38 @@
 
 int main(const int argc, char *argv[])
 {
-    assert(argc >= 2 && "missing parameter!!");
+    assert(argc >= 3 && "missing parameter!!");
 
-    std::ifstream file{argv[1]};
+    std::ifstream dfa_def{argv[1]};
 
-    assert(file.is_open() && "error when opening dfa defination file!!");
+    assert(dfa_def.is_open() && "error when opening dfa defination file!!");
 
     std::vector<DFARaw> dfa_samples{};
     std::string raw{};
-    while (getline(file, raw))
+    while (getline(dfa_def, raw))
     {
         auto pos = raw.find(':');
         TOKEN_TYPE type = get_enum_by_name(raw.substr(0, pos));
         std::string src{raw.substr(pos + 1, raw.size() - pos)};
         dfa_samples.push_back({type, src});
     }
-
-
-    const std::string raw_code{"True False abc //123"};
+    dfa_def.close();
 
     const std::unique_ptr<DFA> dfa{new DFA(dfa_samples)};
+    const std::unique_ptr<Lexer> lexer{new Lexer(*dfa)};
+
     dfa->test_dfa();
 
-    const std::unique_ptr<Lexer> lexer{new Lexer(*dfa, raw_code)};
+    std::string raw_code{};
+    std::ifstream code_file{argv[2]};
+    assert(code_file.is_open() && "error when opening code file!!");
+    while (getline(code_file, raw_code))
+    {
+        std::cout << raw_code << std::endl;
+        lexer->add_tokens(raw_code);
+    }
+    code_file.close();
+
     lexer->print_all_tokens();
 
     return 0;
