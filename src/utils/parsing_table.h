582b15b521b16f8c972b7cb5676477576ffc3513
YKSS\huizeyang <huizeyang@66y.com>
2024-07-31 20:53:21 +0800

    feat: add key method declarations

--- src/utils/parsing_table.h
+++ src/utils/parsing_table.h
@@ -5,12 +5,32 @@
 #ifndef PARSING_TABLE_H
 #define PARSING_TABLE_H
 
+#include <string>
+#include <vector>
+#include "Token.h"
 
+class ParsingTable
+{
+public:
+    explicit ParsingTable(std::vector<std::string> &raw);
 
-class parsing_table {
+    [[nodiscard]] auto &get_first_set() const { return first_set; }
 
+    [[nodiscard]] auto &get_follow_set() const { return follow_set; }
+
+private:
+    std::map<std::string, std::vector<std::string>> first_set{};
+    std::map<std::string, std::vector<std::string>> follow_set{};
+
+    std::map<std::string, std::vector<std::string>> bnf{};
+
+    const std::string optional_terminal{"OPTIONAL"};
+
+    void ebnf_2_bnf(std::map<std::string, std::string> ebnf);
+
+    void calculate_first_set();
+    void calculate_follow_set();
 };
 
 
-
-#endif //PARSING_TABLE_H
+#endif // PARSING_TABLE_H
