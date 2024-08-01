eb08da2ceadc2cec7366ffc272395368f6decd29
YKSS\huizeyang <huizeyang@66y.com>
2024-08-01 19:32:53 +0800

    feat: impl EBNF Tree

--- src/utils/ParsingTable.h
+++ src/utils/ParsingTable.h
@@ -26,10 +26,38 @@ private:
 
     const std::string optional_terminal{"OPTIONAL"};
 
-    void ebnf_2_bnf(const std::map<std::string, std::string>& ebnf);
+    void ebnf_2_bnf(const std::map<std::string, std::string> &ebnf);
 
     void calculate_first_set();
     void calculate_follow_set();
+};
+
+class EBNFTree
+{
+public:
+    class EBNFNode
+    {
+    public:
+        explicit EBNFNode(std::string root) : root(std::move(root)){};
+
+        void add_node(const std::string &root) { nodes.emplace_back(root); }
+
+        [[nodiscard]] auto &get_root() const { return root; }
+        [[nodiscard]] auto &get_nodes() const { return nodes; }
+
+    private:
+        std::string root;
+        std::vector<EBNFNode> nodes{};
+    };
+
+    explicit EBNFTree(std::string non_terminal, std::string pattern) :
+        non_terminal(std::move(non_terminal)), root(std::move(pattern)){};
+
+    std::string non_terminal;
+    [[nodiscard]] EBNFNode get_root_node() const { return root; }
+
+private:
+    EBNFNode root;
 };
 
 
