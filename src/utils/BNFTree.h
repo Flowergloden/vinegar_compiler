9c6e04b317f0c6738e49bab2e1d7eb884868d95b
YKSS\huizeyang <huizeyang@66y.com>
2024-08-26 17:13:02 +0800

    feat: impl basic BNFTree

--- src/utils/BNFTree.h
+++ src/utils/BNFTree.h
@@ -10,10 +10,6 @@
 #include <vector>
 
 
-class BNFTree
-{
-};
-
 class BNFNode : public std::enable_shared_from_this<BNFNode>
 {
 public:
@@ -38,6 +34,24 @@ public:
     std::weak_ptr<BNFNode> parent_node{};
 
     std::vector<std::shared_ptr<BNFNode>> nodes;
+};
+
+class BNFTree
+{
+public:
+    explicit BNFTree(std::string non_terminal) : non_terminal(std::move(non_terminal)) {}
+
+    inline static const std::string ROOT_NODE{"ROOT_NODE"};
+    inline static const std::string OR_NODE{"OR_NODE"};
+    inline static const std::string OPTIONAL_NODE{"OPTIONAL_NODE"};
+    inline static const std::string GROUP_NODE{"GROUP_NODE"};
+
+    [[nodiscard]] std::shared_ptr<BNFNode> get_root_node() const { return root_node; }
+
+    const std::string non_terminal;
+
+private:
+    std::shared_ptr<BNFNode> root_node{std::make_shared<BNFNode>(ROOT_NODE.data())};
 };
 
 
