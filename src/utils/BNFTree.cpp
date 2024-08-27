63f481394d1b6814a5d27d7d454a73a5db1a6070
YKSS\huizeyang <huizeyang@66y.com>
2024-08-27 10:59:27 +0800

    feat: impl basic parse && or syntax

--- src/utils/BNFTree.cpp
+++ src/utils/BNFTree.cpp
@@ -3,4 +3,49 @@
 //
 
 #include "BNFTree.h"
-BNFTree::BNFTree(std::string non_terminal, const std::string &pattern) : non_terminal(std::move(non_terminal)) {}
+
+BNFTree::BNFTree(std::string non_terminal, const std::string &pattern) : non_terminal(std::move(non_terminal))
+{
+    std::shared_ptr<BNFNode> node = root_node;
+    std::string buffer;
+
+    for (auto chr = pattern.begin(); chr != pattern.end(); ++chr)
+    {
+        switch (*chr)
+        {
+        case ' ':
+            if (!buffer.empty())
+            {
+                node->add_node(buffer);
+                buffer.clear();
+            }
+            break;
+
+        case '|':
+            if (*node != ROOT_NODE)
+            {
+                assert(!root_node->parent_node.expired() && "parent node incorrectly expired!!");
+                const auto new_node = node->parent_node.lock()->add_node(OR_NODE);
+                new_node->add_node(node);
+                node = new_node;
+            }
+            else
+            {
+                const auto forward_node = node->nodes.back();
+                node->nodes.pop_back();
+                const auto new_node = node->add_node(OR_NODE);
+                new_node->add_node(forward_node);
+                node = new_node;
+            }
+            break;
+        default:
+            buffer.push_back(*chr);
+        }
+    }
+    // ensure no buffered non-terminal left
+    if (!buffer.empty())
+    {
+        node->add_node(buffer);
+        buffer.clear();
+    }
+}
