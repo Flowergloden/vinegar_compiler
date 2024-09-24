2b4e0415c17f86cdfc6c525ca46af2d916ec4d1a
FlowerGolden <1327097302@qq.com>
2024-09-24 18:04:18 +0800

    fix: undefined exception in Bit flag simulator

--- src/utils/BitFlagSimulator.cpp
+++ src/utils/BitFlagSimulator.cpp
@@ -4,32 +4,3 @@
 
 #include "BitFlagSimulator.h"
 
-template <std::size_t N>
-BitFlagSimulator<N>::BitFlagSimulator(std::array<int, N> byte_maximums) : byte_maximums(std::move(byte_maximums))
-{
-    bytes = new std::array<int, N>(0);
-}
-template <std::size_t N>
-int BitFlagSimulator<N>::operator&(std::bitset<N> rhs)
-{
-    int index{0};
-    std::bitset<N> left{1};
-    left = left << N - 1;
-    while (rhs != left)
-    {
-        rhs = rhs << 1;
-        ++index;
-    }
-
-    return bytes[index];
-}
-template <std::size_t N>
-std::array<int, N> BitFlagSimulator<N>::operator++()
-{
-    return bytes;
-}
-template <std::size_t N>
-std::array<int, N> BitFlagSimulator<N>::operator++(int)
-{
-    return bytes;
-}
--- src/utils/BitFlagSimulator.h
+++ src/utils/BitFlagSimulator.h
@@ -13,15 +13,41 @@ template <std::size_t N>
 class BitFlagSimulator
 {
 public:
-    explicit BitFlagSimulator(std::array<int, N> byte_maximums);
+    explicit BitFlagSimulator(std::array<int, N> byte_maximums) : byte_maximums(std::move(byte_maximums)) {}
     int operator&(std::bitset<N> rhs);
     std::array<int, N> operator++(); // Prefix
     std::array<int, N> operator++(int); // Suffix
 
 private:
     std::array<int, N> bytes;
-    std::array<int, N> byte_maximums;
+    std::array<int, N> byte_maximums{};
 };
 
+template <std::size_t N>
+int BitFlagSimulator<N>::operator&(std::bitset<N> rhs)
+{
+    int index{0};
+    std::bitset<N> left{1};
+    left = left << N - 1;
+    while (rhs != left)
+    {
+        rhs = rhs << 1;
+        ++index;
+    }
+
+    return bytes[index];
+}
+
+template <std::size_t N>
+std::array<int, N> BitFlagSimulator<N>::operator++()
+{
+    return bytes;
+}
+
+template <std::size_t N>
+std::array<int, N> BitFlagSimulator<N>::operator++(int)
+{
+    return bytes;
+}
 
 #endif // BITFLAGSIMULATOR_H
